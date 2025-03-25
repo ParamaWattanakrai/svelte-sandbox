0081eada1d9955151cecdd1eeb7eebd733dfcdd8
Parama Wattanakrai <Parama.Wattanakrai@gmail.com>
2025-03-26 04:50:38 +0700

    first commit

---
+++ src/lib/supabaseClient.ts
@@ -0,0 +1,3 @@
+import { createClient } from '@supabase/supabase-js';
+
+  export const supabase = createClient('https://xzoannsmfmxxxvvrucaf.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh6b2FubnNtZm14eHh2dnJ1Y2FmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDI5MTI2MTcsImV4cCI6MjA1ODQ4ODYxN30.9lkspYcj7OZTEIOUCUFN-KloAuKoozHhNxHG0f98fGs');
