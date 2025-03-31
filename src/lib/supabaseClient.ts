8edbb1c1d756e4794586d897d96996935b30394a
ParamaWattanakrai <Parama.Wattanakrai@gmail.com>
2025-03-31 17:22:16 +0700

    .env setup

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,3 +1,4 @@
+import { PUBLIC_PROJECT_URL, PUBLIC_ANON_KEY } from '$env/static/public';
 import { createClient } from '@supabase/supabase-js';
 
-  export const supabase = createClient('https://xzoannsmfmxxxvvrucaf.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh6b2FubnNtZm14eHh2dnJ1Y2FmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDI5MTI2MTcsImV4cCI6MjA1ODQ4ODYxN30.9lkspYcj7OZTEIOUCUFN-KloAuKoozHhNxHG0f98fGs');
+  export const supabase = createClient(PUBLIC_PROJECT_URL, PUBLIC_ANON_KEY);
